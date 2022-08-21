3d01f8b3967ade40386c4856c198e8d6f4b6c7db
Ridha Watanabe <xsjark@protonmail.com>
2022-08-20 20:40:00 -0500

    finished

---
+++ main.js
@@ -0,0 +1,100 @@
+const numberContainer = document.querySelector("#number-container");
+const operationContainer = document.querySelector("#operation-container");
+const sumContainer = document.querySelector("#sum-container");
+
+const buttons = [...Array(10).keys()].reverse();
+
+const operations = [
+  {
+    label: "+",
+    operation: (a, b) => a + b,
+  },
+  {
+    label: "-",
+    operation: (a, b) => a - b,
+  },
+  {
+    label: "x",
+    operation: (a, b) => a * b,
+  },
+  {
+    label: "/",
+    operation: (a, b) => a / b,
+  },
+];
+
+let obj = {
+  a: [],
+  operation: null,
+  label: "",
+  b: [],
+};
+
+let answer = 0;
+
+const numberMapper = (number) => {
+  if (!obj.operation) {
+    obj.a.push(number);
+    console.log(obj);
+  } else {
+    obj.b.push(number);
+    console.log(obj);
+  }
+  renderSum();
+};
+
+const operationMapper = (operation, label) => {
+  if (obj.a.length > 0) {
+    obj.operation = operation;
+    obj.label = label;
+    console.log(obj);
+  }
+  renderSum();
+};
+
+const renderNumberButtons = () => {
+  buttons.map((button) => {
+    const number_button = document.createElement("button");
+    number_button.classList.add("button");
+    number_button.textContent = button;
+    number_button.addEventListener("click", () => numberMapper(button));
+    numberContainer.appendChild(number_button);
+  });
+};
+
+const renderOperationsButtons = () => {
+  operations.map((operation) => {
+    const operation_button = document.createElement("button");
+    operation_button.classList.add("button");
+    operation_button.textContent = operation.label;
+    operation_button.addEventListener("click", () =>
+      operationMapper(operation.operation, operation.label)
+    );
+    operationContainer.appendChild(operation_button);
+  });
+};
+
+const renderSum = () => {
+  sumContainer.textContent = obj.a + (obj.label || "") + (obj.b || "");
+};
+const renderAnswer = () => {
+  sumContainer.textContent = obj.a + obj.label + obj.b + "=" + answer;
+};
+
+const clearAnswer = () => {
+  sumContainer.textContent = "";
+};
+
+const sum = () => {
+  answer = obj.operation(parseInt(obj.a.join("")), parseInt(obj.b.join("")));
+  renderAnswer();
+};
+
+const clearSum = () => {
+  obj = { a: [], operation: null, b: [] };
+  clearAnswer();
+};
+
+renderNumberButtons();
+renderOperationsButtons();
+renderSum();
